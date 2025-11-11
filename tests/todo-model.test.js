9b87c439a1270370fa0c0f4ed98c6bb2957ae7ec
Jackson Kelley <jacksonkelley@sandiego.edu>
2025-11-11 07:35:28 -0800

    Some additional testing

--- tests/todo-model.test.js
+++ tests/todo-model.test.js
@@ -36,7 +36,29 @@ test('TodoModel - addTodo should add a new todo', () => {
   assert.strictEqual(model.todos.length, 1);
   assert.strictEqual(model.todos[0].text, 'Test todo');
   assert.strictEqual(model.todos[0].completed, false);
+
+  model.addTodo('Test todo 2');
+
+  assert.strictEqual(model.todos.length, 2, 'Should have 2 todos now');
+  assert.strictEqual(model.todos[1].text, 'Test todo 2', 'Second todo text should be correct');
+  assert.strictEqual(model.todos[1].completed, false, 'Second todo should be incomplete');
 });
+test('TodoModel - edited messages should be saved',()=>{
+  const storage = new MockStorage();
+  const model = new TodoModel(storage);
+
+  model.addTodo('Test todo');
+
+  assert.strictEqual(model.todos.length, 1);
+  assert.strictEqual(model.todos[0].text, 'Test todo');
+  assert.strictEqual(model.todos[0].completed, false);
+
+  model.updateTodo(1, 'Test todo (edited)');
+
+  assert.strictEqual(model.todos.length, 1);
+  assert.strictEqual(model.todos[0].text, 'Test todo (edited)');
+  assert.strictEqual(model.todos[0].completed, false);
+})
 
 test('TodoModel - should not add empty todos', () => {
   const storage = new MockStorage();
@@ -46,6 +68,41 @@ test('TodoModel - should not add empty todos', () => {
   model.addTodo('   ');
 
   assert.strictEqual(model.todos.length, 0);
+});
+
+test('TodoModel - updateDueDate should set the due date of a todo', () => {
+  const storage = new MockStorage();
+  const model = new TodoModel(storage);
+  const dueDate = '2025-12-31';
+
+  model.addTodo('Task with a deadline'); 
+
+  model.updateDueDate(1, dueDate); 
+
+  assert.strictEqual(model.todos.length, 1);
+  
+  const updatedTodo = model.todos[0];
+
+  assert.strictEqual(updatedTodo.dueDate, dueDate, 'The todo due date should be updated');
+  assert.strictEqual(updatedTodo.text, 'Task with a deadline', 'Text should remain the same');
+  assert.strictEqual(updatedTodo.completed, false, 'Completed status should remain false');
+});
+test('TodoModel - updateImportance should set the severity level of a todo', () => {
+  const storage = new MockStorage();
+  const model = new TodoModel(storage);
+  const importanceLevel = 'high-value'; 
+
+  model.addTodo('Urgent Task'); 
+
+  model.updateImportance(1, importanceLevel); 
+
+  assert.strictEqual(model.todos.length, 1);
+  
+  const updatedTodo = model.todos[0];
+
+  assert.strictEqual(updatedTodo.importance, importanceLevel, 'The todo importance/severity should be updated');
+  assert.strictEqual(updatedTodo.text, 'Urgent Task', 'Text should remain the same');
+  assert.strictEqual(updatedTodo.completed, false, 'Completed status should remain false');
 });
 
 
